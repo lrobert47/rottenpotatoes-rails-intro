8230fa3c5724c74552f4c59631136b1e778dd169
lrobert47 <lrobert4@citadel.edu>
2018-10-29 22:21:55 +0000

    Im done playing with this

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,9 +12,9 @@ class MoviesController < ApplicationController
   
 
   def index
+    @movies = Movie.all
     @list_ratings = {'G' => '0','PG' => '1', 'PG-13' => '2', 'R' => '3'}
     @all_ratings = Movie.all_ratings
-    @movies = Movie.all
     if params[:ratings].nil?
       if session[:ratings].nil?
         @selected_ratings = @all_ratings
@@ -44,7 +44,7 @@ class MoviesController < ApplicationController
       @title_header = 'hilite'
       @movies = Movie.where(:rating => @selected_ratings.keys).order(title: :asc)
     end
-    if sort_option == 'release_date'  
+    if sort_option == 'release'  
       @release_header = 'hilite'
       @movies = Movie.where(:rating => @selected_ratings.keys).order(release_date: :asc)
     end
@@ -78,32 +78,4 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 end
-
-  def new
-    # default: render 'new' template
-  end
-
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
-
-  def edit
-    @movie = Movie.find params[:id]
-  end
-
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
-
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
 
